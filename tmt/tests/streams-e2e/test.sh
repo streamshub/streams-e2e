0693c057a2045ecaecf72d070da54909b7b278f2
David Kornel <kornys@outlook.com>
2024-08-12 17:56:23 +0200

    Add tmt and packit ci configuration (#18)
    Signed-off-by: David Kornel <kornys@outlook.com>

---
+++ tmt/tests/streams-e2e/test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -eux
+
+# Move to root folder of streams-e2e
+cd ../../../
+
+#get install files
+./mvnw install -P get-operator-files
+
+#run tests
+./mvnw verify -P test -Dgroups="${TEST_GROUPS}"
