5abb9242ee2107ff1b9beab1c3707abc3c0c1933
David Kornel <kornys@outlook.com>
2024-09-03 18:13:30 +0200

    Add FlinkDeploymentTemplate to get default deployment with sql-runner
    Signed-off-by: David Kornel <kornys@outlook.com>

---
+++ src/main/java/io/streams/operands/flink/templates/FlinkDeploymentTemplate.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright streamshub authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.streams.operands.flink.templates;
+
+import org.apache.flink.v1beta1.FlinkDeploymentBuilder;
+import org.apache.flink.v1beta1.FlinkDeploymentSpec;
+import org.apache.flink.v1beta1.flinkdeploymentspec.Job;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * FlinkDeployment templates
+ */
+public class FlinkDeploymentTemplate {
+
+    /**
+     * Return default flink deployment for sql runner
+     *
+     * @param namespace namespace of flink deployment
+     * @param name      name of deployment
+     * @param args      args for sql runner
+     * @return flink deployment builder
+     */
+    public static FlinkDeploymentBuilder defaultFlinkDeployment(String namespace, String name, List<String> args) {
+        return new FlinkDeploymentBuilder()
+            .withNewMetadata()
+                .withName(name)
+                .withNamespace(namespace)
+            .endMetadata()
+            .withNewSpec()
+                .withImage("quay.io/streamshub/flink-sql-runner:latest")
+                .withFlinkVersion(FlinkDeploymentSpec.FlinkVersion.v1_19)
+                .withFlinkConfiguration(
+                    Map.of("taskmanager.numberOfTaskSlots", "1")
+                )
+                .withServiceAccount("flink")
+                .withNewPodTemplate()
+                    .withKind("Pod")
+                    .withNewMetadata()
+                        .withName(name)
+                    .endFlinkdeploymentspecMetadata()
+                    .withNewSpec()
+                        .addNewContainer()
+                            .withName("flink-main-container")
+                            .withImage("quay.io/streamshub/flink-sql-runner:latest")
+                            .addNewVolumeMount()
+                                .withName("product-inventory-vol")
+                                .withMountPath("/opt/flink/data")
+                            .endFlinkdeploymentspecVolumeMount()
+                            .addNewVolumeMount()
+                                .withName("flink-logs")
+                                .withMountPath("/opt/flink/log")
+                            .endFlinkdeploymentspecVolumeMount()
+                            .addNewVolumeMount()
+                                .withName("flink-artifacts")
+                                .withMountPath("/opt/flink/artifacts")
+                            .endFlinkdeploymentspecVolumeMount()
+                        .endFlinkdeploymentspecContainer()
+                        .addNewVolume()
+                            .withName("product-inventory-vol")
+                            .withNewConfigMap()
+                                .withName("product-inventory")
+                                .addNewItem()
+                                    .withKey("productInventory.csv")
+                                    .withPath("productInventory.csv")
+                                .endFlinkdeploymentspecItem()
+                            .endFlinkdeploymentspecConfigMap()
+                        .endFlinkdeploymentspecVolume()
+                        .addNewVolume()
+                            .withName("flink-logs")
+                            .withNewEmptyDir()
+                            .endFlinkdeploymentspecEmptyDir()
+                        .endFlinkdeploymentspecVolume()
+                        .addNewVolume()
+                            .withName("flink-artifacts")
+                            .withNewEmptyDir()
+                            .endFlinkdeploymentspecEmptyDir()
+                        .endFlinkdeploymentspecVolume()
+                    .endFlinkdeploymentspecSpec()
+                .endPodTemplate()
+                .withNewJobManager()
+                    .withNewResource()
+                        .withCpu(1.0)
+                        .withMemory("2048m")
+                    .endResource()
+                .endJobManager()
+                .withNewTaskManager()
+                    .withNewResource()
+                        .withCpu(1.0)
+                        .withMemory("2048m")
+                    .endTaskmanagerResource()
+                .endTaskManager()
+                .withNewJob()
+                    .withJarURI("local:///opt/flink/usrlib/flink-sql-runner.jar")
+                    .withParallelism(1L)
+                    .withUpgradeMode(Job.UpgradeMode.stateless)
+                    .withArgs(args)
+                .endJob()
+            .endSpec();
+    }
+}
--- src/test/java/io/streams/e2e/flink/sql/SqlExampleST.java
+++ src/test/java/io/streams/e2e/flink/sql/SqlExampleST.java
@@ -5,10 +5,11 @@ import io.fabric8.kubernetes.api.model.HasMetadata;
 import io.fabric8.kubernetes.api.model.NamespaceBuilder;
 import io.skodjob.testframe.executor.ExecResult;
 import io.skodjob.testframe.resources.KubeResourceManager;
+import io.streams.constants.FlinkConstants;
 import io.streams.constants.TestConstants;
 import io.streams.e2e.Abstract;
 import io.streams.operands.apicurio.templates.ApicurioRegistryTemplate;
-import io.streams.operands.flink.resoruces.FlinkDeploymentType;
+import io.streams.operands.flink.templates.FlinkDeploymentTemplate;
 import io.streams.operands.flink.templates.FlinkRBAC;
 import io.streams.operands.strimzi.templates.KafkaNodePoolTemplate;
 import io.streams.operands.strimzi.templates.KafkaTemplate;
@@ -105,9 +106,8 @@ public class SqlExampleST extends Abstract {
         KubeResourceManager.getInstance().createOrUpdateResourceWithWait(dataApp.toArray(new HasMetadata[0]));
 
         // Deploy flink
-        FlinkDeployment flinkApp = new FlinkDeploymentType().getClient()
-            .load(exampleFiles.resolve("flink-deployment.yaml").toFile()).item();
-        flinkApp.getMetadata().setNamespace(namespace);
+        FlinkDeployment flinkApp = FlinkDeploymentTemplate.defaultFlinkDeployment(namespace,
+            "recommendation-app", List.of(FlinkConstants.TEST_SQL_EXAMPLE_STATEMENT)).build();
         KubeResourceManager.getInstance().createOrUpdateResourceWithWait(flinkApp);
 
         // Run internal consumer and check if topic contains messages
