e56a5ef354451244f1804bf00def9d3c7a6e3481
David Kornel <kornys@outlook.com>
2025-03-03 09:23:11 +0100

    Update spotbugs and resolve related issues (#112)
    Signed-off-by: David Kornel <kornys@outlook.com>

--- src/main/java/io/streams/operands/flink/resoruces/FlinkDeploymentType.java
+++ src/main/java/io/streams/operands/flink/resoruces/FlinkDeploymentType.java
@@ -4,7 +4,6 @@
  */
 package io.streams.operands.flink.resoruces;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import io.fabric8.kubernetes.api.model.KubernetesResourceList;
 import io.fabric8.kubernetes.client.dsl.MixedOperation;
 import io.fabric8.kubernetes.client.dsl.Resource;
@@ -59,7 +58,6 @@ public class FlinkDeploymentType implements ResourceType<FlinkDeployment> {
     }
 
     @Override
-    @SuppressFBWarnings("EC_UNRELATED_TYPES")
     public boolean isReady(FlinkDeployment resource) {
         FlinkDeployment fd = getClient().inNamespace(resource.getMetadata().getNamespace())
             .withName(resource.getMetadata().getName())
